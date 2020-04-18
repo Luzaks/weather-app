6fdafe85f368822e2104cc92b8a4bf9e9bde4da8
Luzaks <kanemekanik@gmail.com>
2020-04-17 19:01:39 -0500

    Feat: Add DOM data output for weather description.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
-const img = document.querySelector('img');
-const city = document.getElementById('weater-city');
-fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=1a0a2e83eb8dee05e7317550828823c8', {mode: 'cors'})
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        console.log(response);
-    });
+    const container = document.getElementById('container');
+    const city = document.getElementById('weater-city');
+    const weatherState = document.getElementById('weather-state');
+    const weatherAdvice = document.getElementById('weather-advice');
+
+    const capitalize = (string) => {
+        if (typeof string !== 'string') return '';
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+city.innerText+'&appid=1a0a2e83eb8dee05e7317550828823c8', {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            const {name, weather, } = response;
+            const {main, description, icon} = weather[0];
+
+            weatherState.textContent = `${capitalize(description)}`;
+            if (main === 'Clouds'){
+                weatherAdvice.textContent = `Be prepare with your umbrella`;
+            }
+            console.log(name);
+            console.log(main);
+            console.log(description);
+            console.log(icon);
+        });
+
